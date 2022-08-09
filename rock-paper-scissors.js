8f0dea21e2da745dcae1bc00d3c2cf9977718ff3
Joe Pittman <jpittman94@gmail.com>
2022-08-09 18:44:02 -0400

    Create playRound function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -9,4 +9,32 @@ function getComputerChoice() {
     return choices[randomNumber];
 }
 
-getComputerChoice();
+function playRound(playerChoice, computerChoice) {
+    // pseudocode:
+    // convert playerChoice to lowercase so that their choice is case-insensitive
+    // make comparisons between choices
+    // return message saying who won
+
+    playerChoice = playerChoice.toLowerCase();
+
+    let result;
+
+    if (playerChoice === 'rock' && computerChoice === 'scissors') {
+        result = 'You win, rock beats scissors'
+    } else if (playerChoice === 'paper' && computerChoice === 'rock') {
+        result = 'You win, paper beats rock'
+    } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
+        result = 'You win, scissors beats paper'
+    } else if (playerChoice === computerChoice) {
+        result = 'You tie, you both chose ' + playerChoice;
+    } else {
+        result = 'You lose, ' + computerChoice + ' beats ' + playerChoice;
+    }
+
+    // console.log(result);
+    return result;
+}
+
+const computerChoice = getComputerChoice();
+const playerChoice = 'ROCK';
+playRound(playerChoice,computerChoice);
